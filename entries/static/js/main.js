04b461a6e6436ddedc8bbf9ab6c7c42dbf04d3ea
andredi15 <andredi15@gmail.com>
2024-05-13 13:57:39 +0300

    revised js code for colorcoding

--- entries/static/js/main.js
+++ entries/static/js/main.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', function(closePrice, openPrice){
-    let sum = closePrice - openPrice;
-    let displayObj = document.querySelectorAll('.entry_pnl');
-    
-    displayObj.forEach(function(displayObj) {
-        if(sum < 0)
-            displayObj.style.color = 'red'
-        else
-            displayObj.style.color = 'green'
-    })
-})
 
+document.addEventListener('DOMContentLoaded', function(){
+        let displayObj = document.querySelectorAll('.entry_pnl');
+        
+        displayObj.forEach(function(el) {
+            let pnlValue = parseInt(el.innerText)
+            if(pnlValue < 0) {
+                el.style.color = 'red'
+            } else {
+                el.style.color = 'green'
+            }
+        })
+    })
+    
