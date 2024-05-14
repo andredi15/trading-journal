f1853d61b27a3099828bced8840d192f223c1559
andredi15 <andredi15@gmail.com>
2024-05-14 19:26:57 +0300

    not working--revised js function

--- entries/static/js/main.js
+++ entries/static/js/main.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', function(closePrice, openPrice){
-    let sum = closePrice - openPrice;
+document.addEventListener('DOMContentLoaded', function() {
     let displayObj = document.querySelectorAll('.entry_pnl');
     
-    displayObj.forEach(function(displayObj) {
-        if(sum < 0)
-            displayObj.style.color = 'red'
-        else
-            displayObj.style.color = 'green'
-    })
-})
-
+    displayObj.forEach(function(el) {
+        // Parse the inner text of the '.entry_pnl' element to get the numerical value
+        let pnlValue = parseFloat(el.innerText);
+        
+        if(pnlValue < 0) {
+            el.style.color = 'red';
+        } else {
+            el.style.color = 'green';
+        }
+    });
+});
